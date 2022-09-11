ae47c6a25c3f027e76abbaeabf13f6b0c2df3368
chana sat <77387934+sixsat@users.noreply.github.com>
2022-09-11 17:38:13 +0700

    Rename struct fields and separate HTML from Go

--- cyoa/story.go
+++ cyoa/story.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"path/filepath"
 	"strings"
 )
 
@@ -34,9 +35,9 @@ type Chapter struct {
 }
 
 type handler struct {
-	s      Story
-	t      *template.Template
-	pathFn func(r *http.Request) string
+	story    Story
+	template *template.Template
+	pathFn   func(r *http.Request) string
 }
 
 // Option represents a choice offered at the end of a story
@@ -47,84 +48,25 @@ type Option struct {
 	Chapter string `json:"arc"`
 }
 
-var defaultHandlerTmpl = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Choose Your Own Adventure</title>
-</head>
-<body>
-	<section class="page">
-    	<h1>{{.Title}}</h1>
-    	{{range .Paragraphs}}
-    	    <p>{{.}}</p>
-    	{{end}}
-    	<ul>
-    	{{range .Options}}
-    	    <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-    	{{end}}
-    	</ul>
-	</section>
-	<style>
-		body {
-		    font-family: helvetica, arial;
-		}
-		h1 {
-		    text-align: center;
-		    position: relative;
-		}
-		.page {
-		    width: 80%;
-		    max-width: 500px;
-		    margin: auto;
-		    margin-top: 40px;
-		    margin-bottom: 40px;
-		    padding: 80px;
-		    background: #fffcf6;
-		    border: 1px solid #eee;
-		    box-shadow: 0 10px 6px -6px #777;
-		}
-		ul {
-		    border-top: 1px dotted #ccc;
-		    padding: 10px 0 0 0;
-		    -webkit-padding-start: 0;
-		}
-		li {
-		    padding-top: 10px;
-		}
-		a, a:visited {
-		    text-decoration: none;
-		    color: #6295b5;
-		}
-		a:active, a:hover {
-		    color: #7792a2;
-		}
-		p {
-		    text-indent: 1em;
-		}
-	</style>
-</body>
-</html>
-`
+var (
+	errRuntime = errors.New("invalid memory address or nil pointer dereference")
 
-var errRuntime = errors.New("invalid memory address or nil pointer dereference")
-
-var tmpl *template.Template
+	tmpl     *template.Template
+	tmplPath = "template/cyoaweb/template.html"
+)
 
 func init() {
-	tmpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+	tmpl = template.Must(template.ParseFiles(filepath.Clean(tmplPath)))
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := h.pathFn(r)
-	if chapter, ok := h.s[path]; ok {
-		if h.t == nil {
+	if chapter, ok := h.story[path]; ok {
+		if h.template == nil {
 			log.Fatal(errRuntime)
 		}
 
-		err := h.t.Execute(w, chapter)
+		err := h.template.Execute(w, chapter)
 		if err != nil {
 			log.Printf("%v", err)
 			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
@@ -180,6 +122,6 @@ func SetPathParser(fn func(r *http.Request) string) HandlerOption {
 // be used when rendering stories.
 func SetTemplate(t *template.Template) HandlerOption {
 	return func(h *handler) {
-		h.t = t
+		h.template = t
 	}
 }
